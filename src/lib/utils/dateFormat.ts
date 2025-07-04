4679925dcafdc1c3a85a689d108f1181ac30b422
최유정 <choiyujeong0411@gmail.com>
2025-07-04 23:17:29 +0900

    Fix: 날짜형식 한국어 locale로 변경

--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -1,11 +1,13 @@
 import { format } from "date-fns";
+import { ko } from "date-fns/locale";
 
 const dateFormat = (
   date: Date | string,
-  pattern: string = "dd MMM, yyyy",
+  // pattern: string = "dd MMM, yyyy",
+  pattern: string = "yyyy년 M월 d일",
 ): string => {
   const dateObj = new Date(date);
-  const output = format(dateObj, pattern);
+  const output = format(dateObj, pattern, { locale: ko });
   return output;
 };
 
